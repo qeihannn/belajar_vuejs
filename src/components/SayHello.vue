9ec31c3aae854f5b28e449ff18db1b922d67b640
binggo_qeihannn <binggoqeihan@gmail.com>
2025-07-28 15:47:09 +0700

    computed properties

--- src/components/SayHello.vue
+++ src/components/SayHello.vue
@@ -1,5 +1,8 @@
 <script setup>
 import {reactive} from "vue"
+import { ref } from 'vue';
+import {computed} from 'vue';
+
 
 const person = reactive({
     firstName: "",
@@ -10,16 +13,33 @@ const person = reactive({
 function sayHello(){
         person.firstName = document.getElementById("firstName").value;
         person.lastName = document.getElementById("lastName").value;
+
+}
+
+const fullName = computed(() => {
+    console.log(`fullname called`);
+    return `${person.firstName} ${person.lastName}`;
+})
+
+
+const counter = ref(0);  
+
+function increment(){
+    console.log(`increment called`);
+    counter.value++;
+
 }
 </script>
 
 <template>
 <div>
+   
     <input placeholder="First Name" type="text" id="firstName"/> <br>
     <input placeholder="Last Name" type="text" id="lastName"/> <br>
-    <button v-on:click="sayHello">Say Hello</button>
+    <button v-on:click="sayHello">Say Hello</button><br>
+    <button v-on:click="increment">Increment{{counter}}</button> 
 </div>
-<h1>Hello {{person.firstName}} {{person.lastName}}</h1>
+<h1>Hello {{ fullName}}</h1>
 </template>
 
 <style scoped>
