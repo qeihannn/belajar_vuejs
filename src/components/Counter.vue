4d7abae9afa6e3f3f0cc185482275db62ec646a6
binggo_qeihannn <binggoqeihan@gmail.com>
2025-07-28 13:12:28 +0700

    fitur nextTick pada vue js

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from "vue";
+import {nextTick, ref} from "vue";
 
 console.info(`Load component`);
 let counter = ref({
@@ -7,20 +7,32 @@ let counter = ref({
     name: "qeihannn"
 });
 
-function increment() {
+async function increment() {
     console.info(`Increment counter : ${counter.value.count}`);
     counter.value = {
         name: counter.value.name,
         count: counter.value.count + 1
-     }
+     };
+
+        await nextTick();
+
+        counter.value.count++;
+
+        await nextTick();
+
+        counter.value.count++;
+ 
+        await nextTick();
+
+     console.log("Increment count after nextTick");
 }
 </script>
 
 <template>
 <div>
-    <h1 id="count">Counter {{ counter.name }} :{{counter.count}}</h1>
+    <h1 id="count">Counter {{counter.name}} : {{counter.count}}</h1>
     <button v-on:click="increment">Increment</button>
-</div>
+</div>  
 </template>
 
 <style scoped>
