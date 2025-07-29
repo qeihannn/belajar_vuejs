82f55e8d67fa2a9ba3d680d7cd68cba520bf9c6d
binggo_qeihannn <binggoqeihan@gmail.com>
2025-07-29 09:35:20 +0700

    tambah style

---
+++ src/components/Style.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+import {ref} from "vue"
+
+const red = ref(false)
+const bold = ref(false)
+
+function toggleRed() {
+red.value = !red.value
+}
+
+function toggleBold() {
+    bold.value = !bold.value
+}
+
+</script>
+
+<template>
+<h1 :class="{red: red, bold: bold}">Style</h1>
+<h1 :class="['red', 'bold']">style</h1>
+<button v-on:click="toggleRed">Red</button>
+<button v-on:click="toggleBold">Bold</button>
+</template>
+
+<style scoped>
+.red {
+    color: red;
+}
+
+.bold {
+    text-transform: uppercase;
+    font-weight: bold;
+}
+</style>
---
+++ src/style.js
@@ -0,0 +1,5 @@
+import {createApp} from "vue";
+import Style from "./components/Style.vue";
+
+
+createApp(Style).mount("#app");
